3c315bb5c97ad4a593d7dbed979f1b1b2f79e96b
SreyaJoseph <sreyajoseph2004@gmail.com>
2025-09-26 21:34:20 +0530

    feat: added getLocalIP function

--- server.js
+++ server.js
@@ -104,3 +104,13 @@ wss.on('connection', (socket, req) => {
         }));
     }
     
+    function getLocalIP(){
+        const interfaces = os.networkInterfaces();
+        for(const name of Object.keys(interfaces) ){
+            for(const iface of interfaces[name]){
+                if(iface.family === 'IPv4'  && !iface.internal) return iface.address;
+            }
+            }
+        return 'localhost';
+    }
+
